8e165c8487af722cd905b8a3bc9b39b636e9e43e
SnakerWorker <snakerworker@gmail.com>
2014-10-28 21:34:43 +0800

    add close method

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/DbUtils.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/DbUtils.java
@@ -1,16 +1,15 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class DbUtils {
+
+    private Connection connection;
     private String user = "pos";
     private String password = "admin";
     private String url = "jdbc:mysql://localhost:3306/pos?useUnicode=true&&characterEncoding=utf-8";
 
-    private Connection getConnection(){
-
-        Connection connection = null;
+    public Connection getConnection(){
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -20,4 +19,14 @@ public class DbUtils {
 
         return connection;
     }
+
+    public void close(){
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+        }
+    }
+
 }
