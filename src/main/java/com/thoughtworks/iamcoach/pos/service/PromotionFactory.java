ccbecdd6b43adfd62c0ad721fb99a3f83c2d6129
SnakerWorker <snakerworker@gmail.com>
2014-10-28 21:51:41 +0800

    add test PromotionFactoryTest

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionFactory.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.iamcoach.pos.service;
+
+import com.thoughtworks.iamcoach.pos.model.Promotion;
+
+public class PromotionFactory {
+    public static Promotion getInstance(int type) {
+        return null;
+    }
+}
