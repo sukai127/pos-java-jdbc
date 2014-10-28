06ef4a417039dff531846d98a9b4ca86b03db0aa
SnakerWorker <snakerworker@gmail.com>
2014-10-28 21:30:25 +0800

    add getConnection method

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/DbUtils.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/DbUtils.java
@@ -1,7 +1,23 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
-/**
- * Created by sukai on 10/28/14.
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DbUtils {
+    private String user = "pos";
+    private String password = "admin";
+    private String url = "jdbc:mysql://localhost:3306/pos?useUnicode=true&&characterEncoding=utf-8";
+
+    private Connection getConnection(){
+
+        Connection connection = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+        }
+
+        return connection;
+    }
 }
