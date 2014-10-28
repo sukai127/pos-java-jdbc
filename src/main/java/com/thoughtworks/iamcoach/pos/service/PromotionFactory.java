63356c6f92b8379e6ec1ee7587d3ed243244b738
SnakerWorker <snakerworker@gmail.com>
2014-10-28 21:53:13 +0800

    modify getInstance method

--- src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionFactory.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/PromotionFactory.java
@@ -1,9 +1,29 @@
 package com.thoughtworks.iamcoach.pos.service;
 
-import com.thoughtworks.iamcoach.pos.model.Promotion;
+import com.thoughtworks.iamcoach.pos.model.*;
 
 public class PromotionFactory {
-    public static Promotion getInstance(int type) {
-        return null;
+
+    public static final int BUY_TWO_GET_ONE = 0;
+    public static final int SECOND_HALF_PRICE = 1;
+    public static final int DISCOUNT = 2;
+
+    public static Promotion getInstance(int type){
+        Promotion promotion = null;
+        switch (type) {
+            case Promotion.BUY_TWO_GET_ONE:
+                promotion = new BuyTwoGetOnePromotion();
+                break;
+            case Promotion.SECOND_HALF_PRICE:
+                promotion = new SecondHalfPricePromotion();
+                break;
+            case Promotion.DISCOUNT:
+                promotion = new DiscountPromotion();
+                break;
+            default:
+                break;
+        }
+
+        return promotion;
     }
 }
