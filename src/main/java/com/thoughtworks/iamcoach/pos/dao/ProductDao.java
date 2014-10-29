8317395bd94e511b28cfbb1b50d176660db83156
SnakerWorker <snakerworker@gmail.com>
2014-10-29 19:31:13 +0800

    add test ProductDaoTest

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/ProductDao.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/ProductDao.java
@@ -1,7 +1,13 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
-/**
- * Created by sukai on 10/29/14.
- */
-public class ProductDao {
+import com.thoughtworks.iamcoach.pos.model.Product;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ProductDao extends DbUtils{
+
+    public List<Product> getProductList() throws SQLException {
+        return null;
+    }
 }
