a4d4db58f8abfd907ba65364bf4f6217b8ca8213
SnakerWorker <snakerworker@gmail.com>
2014-10-29 20:30:40 +0800

    add PromotionDao

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDao.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.iamcoach.pos.dao;
+
+import java.util.List;
+
+public class PromotionDao extends DbUtils{
+
+    public List<Integer> getPromotionTypes() {
+        return null;
+    }
+}
