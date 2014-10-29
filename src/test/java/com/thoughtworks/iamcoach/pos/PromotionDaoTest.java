b0215d1774b310f3fa997ee24ccb3941d2b95593
SnakerWorker <snakerworker@gmail.com>
2014-10-29 20:29:40 +0800

    add test should_return_promotionTypes

--- src/test/java/com/thoughtworks/iamcoach/pos/PromotionDaoTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/PromotionDaoTest.java
@@ -1,7 +1,22 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by sukai on 10/29/14.
- */
+import com.thoughtworks.iamcoach.pos.dao.PromotionDao;
+import com.thoughtworks.iamcoach.pos.model.Promotion;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class PromotionDaoTest {
+    @Test
+    public void should_return_promotionTypes() throws SQLException {
+
+        PromotionDao promotionDao = new PromotionDao();
+        List<Integer> list = promotionDao.getPromotionTypes();
+
+        assertThat(list.size()).isEqualTo(3);
+        assertThat(list.get(0)).isEqualTo(0);
+    }
 }
