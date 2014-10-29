9731c21b3a969d9dd5c2c421f6470d12bfa1591e
SnakerWorker <snakerworker@gmail.com>
2014-10-29 20:34:02 +0800

    add getPromotionTypes method

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDao.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/PromotionDao.java
@@ -1,10 +1,32 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PromotionDao extends DbUtils{
 
-    public List<Integer> getPromotionTypes() {
-        return null;
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
+
+    public List<Integer> getPromotionTypes() throws SQLException {
+
+        List<Integer> promotionTypes = new ArrayList<Integer>();
+        String sql = "select * from promotion";
+        
+        connection = getConnection();
+        preparedStatement = connection.prepareStatement(sql);
+        resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()){
+            int type = resultSet.getInt("type");
+            promotionTypes.add(type);
+        }
+
+        return promotionTypes;
     }
 }
