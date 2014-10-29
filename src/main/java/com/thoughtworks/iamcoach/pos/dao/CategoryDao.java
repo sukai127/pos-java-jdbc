463b5bbb264652751a006a499d51ce02dbf71b49
SnakerWorker <snakerworker@gmail.com>
2014-10-29 19:59:31 +0800

    add test should_return_categoryList method

---
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/CategoryDao.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.iamcoach.pos.dao;
+
+import com.thoughtworks.iamcoach.pos.model.Category;
+
+import java.util.List;
+
+/**
+ * Created by sukai on 10/29/14.
+ */
+public class CategoryDao {
+    public List<Category> getCategoryList() {
+        return null;
+    }
+}
--- src/test/java/com/thoughtworks/iamcoach/pos/CategoryDaoTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/CategoryDaoTest.java
@@ -1,7 +1,19 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by sukai on 10/29/14.
- */
+import com.thoughtworks.iamcoach.pos.dao.CategoryDao;
+import com.thoughtworks.iamcoach.pos.model.Category;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class CategoryDaoTest {
+    @Test
+    public void should_return_categoryList(){
+        CategoryDao categoryDao = new CategoryDao();
+        List<Category> list = categoryDao.getCategoryList();
+        assertThat(list.size()).isEqualTo(4);
+        assertThat(list.get(0).getName()).isEqualTo("饮料");
+    }
 }
