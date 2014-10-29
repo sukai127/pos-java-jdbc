caa80fc1c95b3732d5482eb9595b546812131376
SnakerWorker <snakerworker@gmail.com>
2014-10-29 21:48:33 +0800

    modify getCategory method

--- src/main/java/com/thoughtworks/iamcoach/pos/service/ProductService.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.iamcoach.pos.service;
 
+import com.thoughtworks.iamcoach.pos.dao.CategoryDao;
 import com.thoughtworks.iamcoach.pos.dao.ProductDao;
+import com.thoughtworks.iamcoach.pos.model.Category;
 import com.thoughtworks.iamcoach.pos.utils.FileUtils;
 import com.thoughtworks.iamcoach.pos.model.Product;
 import com.thoughtworks.iamcoach.pos.model.Promotion;
@@ -12,66 +14,24 @@ import java.util.List;
 
 public class ProductService {
 
-    private static List<String> buyTwoGetOneList;
-    private static List<String> secondHalfPriceList;
-    private static List<String> discountList;
     private ProductDao productDao = new ProductDao();
+    private CategoryDao categoryDao = new CategoryDao();
 
-    static{
-        try {
-
-            buyTwoGetOneList = FileUtils.get("buy_two_get_one_free_promotion.txt");
-            secondHalfPriceList = FileUtils.get("second_half_price_promotion.txt");
-            discountList = FileUtils.get("discount_promotion.txt");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public Product buildProduct(String productString) throws IOException {
-
-        String []fields = productString.split(",");
-        double price = Double.parseDouble(fields[3]);
-
-        List<Promotion> list = this.getPromotions(fields[0]);
-        Product product = new Product(0,fields[0],fields[1],fields[2],price,null,list);
-        return product;
-    }
-
-    public List<Product> getProductList() throws SQLException, IOException {
+    public List<Product> getProductList() throws SQLException {
 
         List<Product> productList = productDao.getProductList();
 
-        for(Product str : productList){
-            Product product = this.buildProduct("");
+        for(Product product : productList){
+            product.setCategory(this.getCategory(product.getId()));
             productList.add(product);
         }
 
         return productList;
     }
 
-    public List<Promotion> getPromotions(String barcode) throws IOException {
+    private Category getCategory(int id) throws SQLException {
 
-        List<Promotion> promotionList = new ArrayList<Promotion>();
-
-        if(buyTwoGetOneList.contains(barcode)){
-            promotionList.add(PromotionFactory.getInstance(PromotionFactory.BUY_TWO_GET_ONE));
-        }
-        if(secondHalfPriceList.contains(barcode)){
-            promotionList.add(PromotionFactory.getInstance(PromotionFactory.SECOND_HALF_PRICE));
-        }
-        if(isExist(discountList,barcode)){
-            promotionList.add(PromotionFactory.getInstance(PromotionFactory.DISCOUNT));
-        }
-        return promotionList;
+        return categoryDao.getCategory(id);
     }
 
-    private boolean isExist(List<String> discountList, String barcode) {
-        for(String str : discountList){
-            if(str.contains(barcode)){
-                return true;
-            }
-        }
-        return false;
-    }
 }
