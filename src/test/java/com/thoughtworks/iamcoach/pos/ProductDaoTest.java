13c1a6be4b52a6613bf900e20fad562aba0ca9dd
SnakerWorker <snakerworker@gmail.com>
2014-10-29 19:37:23 +0800

    add test should_return_productList method

--- src/test/java/com/thoughtworks/iamcoach/pos/ProductDaoTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/ProductDaoTest.java
@@ -1,7 +1,21 @@
 package com.thoughtworks.iamcoach.pos;
 
-/**
- * Created by sukai on 10/29/14.
- */
+import com.thoughtworks.iamcoach.pos.dao.ProductDao;
+import com.thoughtworks.iamcoach.pos.model.Product;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class ProductDaoTest {
+    @Test
+    public void should_return_productList() throws SQLException {
+        ProductDao productDao = new ProductDao();
+        List<Product> productList = productDao.getProductList();
+        assertThat(productList.size()).isEqualTo(6);
+        assertThat(productList.get(0).getCategory().getName()).isEqualTo("饮料");
+        assertThat(productList.get(1).getPromotions().size()).isEqualTo(3);
+    }
 }
