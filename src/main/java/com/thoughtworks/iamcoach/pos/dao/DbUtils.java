a158803b5257ba7a54ab027103b7921b781fb340
SnakerWorker <snakerworker@gmail.com>
2014-10-30 22:36:39 +0800

    modify getConnection method

--- src/main/java/com/thoughtworks/iamcoach/pos/dao/DbUtils.java
+++ src/main/java/com/thoughtworks/iamcoach/pos/dao/DbUtils.java
@@ -1,22 +1,24 @@
 package com.thoughtworks.iamcoach.pos.dao;
 
+import com.thoughtworks.iamcoach.pos.utils.FileUtils;
+
 import java.sql.*;
 
 public class DbUtils {
 
-    private Connection connection;
-    private String user = "pos";
-    private String password = "admin";
+    private Connection connection = null;
     private String url = "jdbc:mysql://localhost:3306/pos?useUnicode=true&&characterEncoding=utf-8";
 
     public Connection getConnection(){
-
         try {
+            String user = FileUtils.getValue("user");
+            String password = FileUtils.getValue("password");
+
             Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(url, user, password);
         } catch (Exception e) {
+            System.out.println("请检查驱动或在pos.properties里面输入正确的用户名和密码");
         }
-
         return connection;
     }
 
