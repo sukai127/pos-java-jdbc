f18ad2e70c716d7720b524e172cc7a3751ff2d78
SnakerWorker <snakerworker@gmail.com>
2014-10-30 21:42:18 +0800

    modify getCategory method

--- src/test/java/com/thoughtworks/iamcoach/pos/CategoryServiceTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/CategoryServiceTest.java
@@ -4,11 +4,13 @@ import com.thoughtworks.iamcoach.pos.model.Category;
 import com.thoughtworks.iamcoach.pos.service.CategoryService;
 import org.junit.Test;
 
+import java.sql.SQLException;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class CategoryServiceTest {
     @Test
-    public void should_return_category_when_input_productId(){
+    public void should_return_category_when_input_productId() throws SQLException {
         
         CategoryService categryService = new CategoryService();
         Category category = categryService.getCategory(1);
