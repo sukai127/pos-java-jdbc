321e03afd9bad372298295db0d0b8f938bf8dfe0
SnakerWorker <snakerworker@gmail.com>
2014-10-30 21:26:58 +0800

    modify test should_return_12_when_input_cartItem

--- src/test/java/com/thoughtworks/iamcoach/pos/DiscountPromotionTest.java
+++ src/test/java/com/thoughtworks/iamcoach/pos/DiscountPromotionTest.java
@@ -10,7 +10,7 @@ import static org.fest.assertions.api.Assertions.assertThat;
 public class DiscountPromotionTest {
     @Test
     public void should_return_12_when_input_cartItem(){
-        Product product = new Product(1,"ITEM000001","可乐","瓶",4.00,null,null);
+        Product product = new Product(2,"ITEM000001","可乐","瓶",4.00,null,null);
         CartItem cartItem = new CartItem(product,4);
         DiscountPromotion discountPromotion = new DiscountPromotion();
 
